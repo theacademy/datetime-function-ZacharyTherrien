4d6e879994caba4fa9b90393d6f7d7fc9bb5fc13
Zachary Therrien <ZTherrien.work@gmail.com>
2026-05-01 00:30:41 -0400

    Commit1

--- dateTimeFunction/src/main/java/com/example/DateTimeFunctions.java
+++ dateTimeFunction/src/main/java/com/example/DateTimeFunctions.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import java.time.*;
+import java.time.chrono.ChronoLocalDate;
 
 public class DateTimeFunctions {
 
@@ -22,7 +23,7 @@ public class DateTimeFunctions {
 	 */
 	public static LocalDate getTodaysDate() {
 		//YOUR CODE STARTS HERE
-		return null;
+		return LocalDate.now();
 		//YOUR CODE ENDS HERE
 
 	}
@@ -33,7 +34,7 @@ public class DateTimeFunctions {
 	 */
 	public static LocalDate getLaterDatebyDays(LocalDate date, int x) {
 		//YOUR CODE STARTS HERE
-		return null;
+		return date.plusDays(x);
 		//YOUR CODE ENDS HERE
 
 	}
@@ -44,7 +45,7 @@ public class DateTimeFunctions {
 	 */
 	public static LocalDate getPreviousDatebyWeeks(LocalDate date, int x) {
 		//YOUR CODE STARTS HERE
-		return null;
+		return date.plusWeeks(-x);
 		//YOUR CODE ENDS HERE
 
 	}
@@ -56,7 +57,9 @@ public class DateTimeFunctions {
 	 */
 	public static String getTimeDifference(LocalDate date1, LocalDate date2) {
 		//YOUR CODE STARTS HERE
-		return null;
+		return "Years-" + Period.between(date1, date2).getYears() +
+				":Months-" + Period.between(date1, date2).getMonths() +
+				":Days-" + Period.between(date1, date2).getDays();
 		//YOUR CODE ENDS HERE
 
 	}
